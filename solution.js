46187ad667eb7fa5c3758645f85a404afe9ccad2
Ghate Suyog <suyogghateminks@gmail.com>
2022-07-26 15:36:32 +0530

    completed

--- solution.js
+++ solution.js
@@ -2,6 +2,7 @@
 let yourArray;
 // assign your array the correct value in the next line
 // your code here!
+yourArray = ["suyog", "ghate", 5, 6, {one: 1, two: 2}, {three: 3, four: 4}, true, false]
 // code end
 
 module.exports = yourArray
